88fc954108f25774f71c863cfc5ecad9e9658a67
Jose Miranda <jose.miranda@lexmark.com>
2019-01-20 18:35:25 -0500

    system-fingerprint: use sysinfo and partiton-info crates to get disk uuid

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate partition_identity;
+extern crate sysinfo;
+use partition_identity::{PartitionID, PartitionSource};
+use self::PartitionSource::*;
+use sysinfo::{System, SystemExt, DiskExt};
+
 fn main() {
-    println!("Hello, world!");
+
+    let syst = System::new();
+
+    let disk = syst.get_disks();
+    let partition = &disk[0].get_name().to_str().unwrap();
+    let path = format!("/dev/{}", partition); 
+
+    let disk_uuid = PartitionID::get_source(UUID, &path).map(|id| id.id).unwrap();
+    println!("UUID: {}", disk_uuid);
+
 }
