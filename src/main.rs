a711a3ca40e10c3a54f3d534b428b8a062fdf835
Jose Miranda <jose.miranda@lexmark.com>
2019-01-21 18:11:17 -0500

    system-fingerprint: Use uname crate to create second fingerprint

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,29 @@ use partition_identity::{PartitionID, PartitionSource};
 use self::PartitionSource::*;
 use sysinfo::{System, SystemExt, DiskExt};
 use sha2::{Sha256, Digest};
-
+use uname::uname;
 
 fn main() {
-
+    // Assuming it is a Linux-based system 
+    // Let't try to get the disk info
     let syst = System::new();
-
     let disk = syst.get_disks();
     let partition = &disk[0].get_name().to_str().unwrap();
     let path = format!("/dev/{}", partition); 
 
     let disk_uuid = PartitionID::get_source(UUID, &path).map(|id| id.id).unwrap();
-    println!("UUID:{}", disk_uuid);
 
     let mut hasher = Sha256::new();
     hasher.input(disk_uuid.as_bytes());
-    let result = format!("{:X}",hasher.result());
-    println!("Fingerprint: {}", result);
+    let mut result = format!("{:X}", hasher.result());
+    println!("First Fingerprint (Disk's UUID): {}", result);
+
+    let info = uname().unwrap();
+
+    let mut hasher = Sha256::new();
+    let second_hash = format!("{}-{}", disk_uuid, info.version);
+    hasher.input(second_hash.as_bytes());
+    result = format!("{:X}",hasher.result());
+    println!("Second fingerprint (Disk's UUID with some uname info) {}", result);
+
 }
